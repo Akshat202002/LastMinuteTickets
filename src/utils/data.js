07dff14cce9b83ab6097df6d877d4182776be86a
Abhijeet Chouhan <abhi.kkr.chauhan@gmail.com>
2023-04-09 17:04:37 +0530

    images

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,34 +1,34 @@
-import I1 from "../img/i1.png";
-import F1 from "../img/f1.png";
-import C3 from "../img/c3.png";
-import Fi1 from "../img/fi1.png";
+import I1 from "../img/ticket1.jpg";
+import F1 from "../img/ticket2.jpg";
+import C3 from "../img/ticket3.jpg";
+import Fi1 from "../img/ticket4.jpg";
 
 export const heroData = [
   {
     id: 1,
-    name: "Icecream",
-    decp: "Chocolate & vanilla",
-    price: "5.25",
+    name: "Music",
+    decp: "starting from",
+    price: " 5.25",
     imageSrc: I1,
   },
   {
     id: 2,
-    name: "Strawberries",
-    decp: "Fresh Strawberries",
+    name: "Food",
+    decp: "starting from",
     price: "10.25",
     imageSrc: F1,
   },
   {
     id: 3,
-    name: "Chicken Kebab",
-    decp: "Mixed Kebab Plate",
+    name: "Sports",
+    decp: "starting from",
     price: "8.25",
     imageSrc: C3,
   },
   {
     id: 4,
-    name: "Fish Kebab",
-    decp: "Mixed Fish Kebab",
+    name: "Dance",
+    decp: "starting from",
     price: "5.25",
     imageSrc: Fi1,
   },
